fc1342213c5cb5139f8655089328619f9e1fcea4
MohammadAbbasi76 <work.abbasii@gmail.com>
2025-04-23 15:43:02 +0330

    Clean up main.cpp: remove commented lines for clarity

--- main.cpp
+++ main.cpp
@@ -2,20 +2,19 @@
 #include "EventHandler.hpp"
 
 int main() {
-    // Initialize EventHandler (Elden Tree)
+
     EventHandler handler;
 
-    // Register some gods
+
     handler.registerGod("Zeus");
     handler.registerGod("Thor");
     handler.registerGod("Poseidon");
 
-    // Submit some events
     handler.submitEvent("Zeus", Event("Thunder Strike", "Strike the enemy"));
     handler.submitEvent("Thor", Event("Hammer Swing", "Smash the rock"));
     handler.submitEvent("Poseidon", Event("Tsunami", "Flood the village"));
 
-    // Process events in round-robin fashion
+
     while (handler.hasPendingEvents()) {
         Event event = handler.getNextEvent();
         // Simulate event handling (printing)
