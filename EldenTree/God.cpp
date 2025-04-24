d27e77f66870f6af894088d2d6c5437d6602f6e4
MohammadAbbasi76 <work.abbasii@gmail.com>
2025-04-25 02:32:07 +0330

    Remove commented-out postEvent template method from God.cpp

--- EldenTree/God.cpp
+++ EldenTree/God.cpp
@@ -2,13 +2,3 @@
 
 
 int God::GodId = 0;
-
-// template <typename eventType>
-// bool God::postEvent(const eventType &eventData)
-// {
-//     Event<eventType> event(GodId, eventData);
-
-//     // Assuming EldenTree is a singleton or globally accessible
-//     // EldenTree::getInstance().postEvent(Name, event);
-//     return true;
-// }
